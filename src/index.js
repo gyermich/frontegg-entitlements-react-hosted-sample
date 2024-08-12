42b5529a733a201bf8e9d9869aa2348fcd62efd6
Galina Yermicheva <galina@angaza.com>
2024-08-12 12:00:14 -0400

    Match sample app to the Angaza one

--- src/index.js
+++ src/index.js
@@ -1,30 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom'; // For react 17
-// For react 18: import ReactDOM from 'react-dom/client';
-import App from './App';
 import './index.css';
 
+// For react 18: import ReactDOM from 'react-dom/client';
+import App from './App';
 import { FronteggProvider } from '@frontegg/react';
+import React from 'react';
+import ReactDOM from 'react-dom'; // For react 17
 
 const contextOptions = {
-  baseUrl: 'https://[YOUR_SUBDOMAIN].frontegg.com',
-  clientId: '[YOUR-CLIENT-ID]'
+  baseUrl: 'https://app-k4qik94aul5b.us.frontegg.com',
+  clientId: '2ad54ecb-c5b6-4bf0-946c-45ece4cc3f6f',
+  hostedLoginOptions: {
+        loadUserOnFirstLoad: true,
+    },
 };
 
 
 const authOptions = {
- // keepSessionAlive: true // Uncomment this in order to maintain the session alive
+  keepSessionAlive: true,
+  // routes: { hostedLoginRedirectUrl: `${process.env.UI_PATH_PREFIX}/oauth/callback` },
+
 };
 
-// For react 18: 
+// For react 18:
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(
 ReactDOM.render(
-    <FronteggProvider 
+    <FronteggProvider
       contextOptions={contextOptions}
       hostedLoginBox={true}
       authOptions={authOptions}
-      entitlementsOptions={{ enabled: true }}
+    // history={history}
     >
         <App />
     </FronteggProvider>,
